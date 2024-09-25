1cafda0c0bbf6dd42b788db82427dcf4c5c9af3e
Emy sheta <emysheta1682@gmail.com>
2024-09-25 20:24:30 +0200

    initial commit

---
+++ lib/Features/Auth/Presntations/VIew_Model/Login_viewModel.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginViewmodel extends StatelessWidget {
+  const LoginViewmodel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
